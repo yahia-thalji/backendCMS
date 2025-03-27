6dac54b16d754e1565123056a8ae02781e107ed8
yahyathalji <yahyajaradatssss@gmail.com>
2025-03-27 22:09:52 +0200

    backend: 1- added product controller

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,6 +1,7 @@
 import {RequestHandler} from 'express'
 import { Product } from '../entities/product';
 import { Category } from '../entities/category';
+import { ProgramUpdateLevel } from 'typescript/lib/typescript';
 
 export const createProduct:RequestHandler = async (req,res):Promise<any> => {
     try {
@@ -11,6 +12,9 @@ export const createProduct:RequestHandler = async (req,res):Promise<any> => {
         const categoryIsExist = await Category.findOneBy({categoryId});
         if(!categoryIsExist){
             return res.status(400).json({message:"Not Found Category"})
+        }
+        if(quantity <0){
+            return res.status(400).json({message:"quantity must be positive number"})
         }
         const addProduct = Product.create({
             name,
@@ -29,6 +33,124 @@ export const createProduct:RequestHandler = async (req,res):Promise<any> => {
         return res.status(200).json(addProduct );
     } catch (error:any) {
         console.log("Error in createProduct controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+
+export const updateProduct:RequestHandler = async (req,res):Promise<any> => {
+    try {
+        const productId :any= req.params.productId;
+        const product= await Product.findOneBy({productId});
+        if(!product){
+            return res.status(400).json({messaga:"Not Found Product"});
+        }
+        const {name , description , quantity ,categoryId,price ,newPrice}=req.body;
+        if(categoryId){
+        const categoryIsExist = await Category.findOneBy({categoryId});
+        if(!categoryIsExist){
+            return res.status(400).json({message:"Not Found Category"})
+        }else{
+            product.category=categoryIsExist;
+        }
+    }
+        if(name) product.name = name;
+        if(description) product.description=description;
+        if(quantity && quantity<0) {
+            return res.status(400).json({message :"if want change the quantity be must positive number"})
+        }else{
+            product.quantity=quantity;
+        }
+        if(price && price >0) product.price=price;
+        if (newPrice) {
+            if (product.price && newPrice >= product.price) {
+                return res.status(400).json({ message: "New price must be lower than the original price" });
+            }
+            product.newPrice = newPrice;
+        }
+        await product.save();
+        return res.status(200).json(product);
+    } catch (error:any) {
+        console.log("Error in updateProduct controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const deleteProduct:RequestHandler = async(req ,res):Promise <any> =>{
+    try {
+        const productId :any = req.params.productId;
+        const product = await Product.findOneBy({productId});
+        if(!product){
+            return res.status(400).json({message : "Not Found Product"});
+        }
+        await product.remove();
+        return res.status(200).json({message:"The deletion process has been completed"})
+    } catch (error:any) {
+        console.log("Error in deleteProduct controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const getProduct:RequestHandler = async(req ,res ):Promise<any> =>{
+    try {
+        const productId :any = req.params.productId;
+        const product = await Product.findOneBy({productId});
+        if(!product){
+            return res.status(400).json({message:"Not Found Product"});
+        }
+        return res.status(200).json(product);
+    } catch (error:any) {
+        console.log("Error in getProduct controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const getAllProducts:RequestHandler = async(req ,res):Promise<any> =>{
+    try {
+        const products = await Product.find();
+        if(!products){
+            return res.status(400).json({message:"Not Found Products"})
+        }
+        if(products.length===0){
+            return res.status(200).json({message:"No Product Yet"})
+        }
+        return res.status(200).json(products);
+    } catch (error:any) {
+        console.log("Error in getAllProducts controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const categoryWithProduct:RequestHandler = async (req ,res):Promise<any> => {
+    try {
+        const categoryWithProduct = await Product.find({relations:['category']});
+        if(!categoryWithProduct){
+            return res.status(400).json({message:"Not Found categoryWithProduct"})
+        }
+        if(categoryWithProduct.length===0){
+            return res.status(200).json({message:"No Product Yet"})
+        }
+        return res.status(200).json(categoryWithProduct);
+    } catch (error:any) {
+        console.log("Error in categoryWithProduct controller", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+
+export const categoryIdWithAllProducts:RequestHandler = async(req ,res):Promise<any> =>{
+    try {
+        const categoryId :any=req.params.categoryId;
+
+        const category = await Category.find({where:{categoryId:categoryId} ,relations:['product']});
+        if(!category){
+            return res.status(400).json({message:"Not Found category"});
+        }
+
+        return res.status(200).json(category);
+        
+    } catch (error:any) {
+        console.log("Error in pIdCId controller", error.message);
         res.status(500).json({error: "Internal server error"});
     }
 }
--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,19 +1,19 @@
 import express from 'express'
-import { createProduct } from '../controller/productController';
+import { categoryIdWithAllProducts, categoryWithProduct, createProduct, deleteProduct, getAllProducts, getProduct, updateProduct } from '../controller/productController';
 
 const router = express.Router();
 
 router.post("/createProduct",createProduct);
 
-router.post("/update/:productId",);
-router.delete("delete/:productId",);
+router.post("/update/:productId",updateProduct);
+router.delete("/delete/:productId",deleteProduct);
 
 //get single product
-router.get('/:ProductId', );
-router.get("/categoryWithProduct");
+router.get('/getProduct/:productId', getProduct);
+router.get("/categoryWithProduct",categoryWithProduct);
 
-router.get("/getAllProducts",);
+router.get("/getAllProducts",getAllProducts);
 
-router.get("/categoryId/AllProduct",);
+router.get("/:categoryId/AllProduct",categoryIdWithAllProducts);
 
 export default router;
