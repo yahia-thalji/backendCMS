07dbb0fd3d15f07588c7c3421346a8c9eae8891f
yahia-thalji <yahiathalji@gmail.com>
2025-11-21 17:43:25 +0200

    update port

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ import reportRouter from "./routes/reportsRoutes";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 
 app.use(cors({
