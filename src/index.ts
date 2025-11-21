655703ddcae3127edf4ed71fd57899f6786e09b8
yahia-thalji <yahiathalji@gmail.com>
2025-11-21 16:58:20 +0200

    add domian

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "frontendcms-production.up.railway.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization", "cookie"],
